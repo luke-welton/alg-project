d9425fe1371c9fea463e426a8d56f36a1f1d5648
Luke Welton <lmoowelton@gmail.com>
2018-06-28 21:00:20 -0500

    wrote algorithms

---
+++ algorithms.h
@@ -0,0 +1,4 @@
+int algorithm1 (int * x[], int p, int q);
+int algorithm2 (int * x[], int p, int q);
+int algorithm3 (int * x[], int l, int u);
+int algorithm4 (int * x[], int p, int q);
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "algorithms.h"
+
+int main() {
+
+}
