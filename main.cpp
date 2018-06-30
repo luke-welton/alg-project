30172cb26b10ae12277c9cee76c73da375f0a40f
Luke Welton <lmoowelton@gmail.com>
2018-06-30 15:10:57 -0500

    add randomized performance testing

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <sstream>
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
 #include "algorithms.h"
 using namespace std;
 
 void presetAlgs();
+void randomizedAlgs();
 
 int main() {
     presetAlgs();
+    randomizedAlgs();
 }
 
 void presetAlgs() {
@@ -33,4 +36,62 @@ void presetAlgs() {
         printf("An error occurred trying to open the file \"phw_input.txt\"");
         exit(1);
     }
+}
+
+void randomizedAlgs() {
+    srand(time(NULL));
+
+    int* arrays[19];
+    for (int i = 0; i < 19; i++) {
+        int newArray[5 * (i + 1)];
+        for (int j = 0; j < 5 * (i + 1); j++) {
+            int multiplier = rand() % 2 == 1 ? -1 : 1;
+            newArray[j] = multiplier * (rand() % 50);
+        }
+
+        arrays[i] = newArray;
+    }
+
+    double times[19][4];
+    for (int i = 0; i < 19; i++) {
+        clock_t start, end;
+
+        start = clock();
+        for (int t = 0; t < 25000; t++) {
+            algorithm1(arrays[i], 0, 5 * (i + 1));
+        }
+        end = clock();
+        times[i][0] = (end, start) / (double) CLOCKS_PER_SEC;
+
+
+        start = clock();
+        for (int t = 0; t < 25000; t++) {
+            algorithm2(arrays[i], 0, 5 * (i + 1));
+        }
+        end = clock();
+        times[i][1] = (end, start) / (double) CLOCKS_PER_SEC;
+
+
+        start = clock();
+        for (int t = 0; t < 25000; t++) {
+            algorithm3(arrays[i], 0, 5 * (i + 1));
+        }
+        end = clock();
+        times[i][2] = (end, start) / (double) CLOCKS_PER_SEC;
+
+
+        start = clock();
+        for (int t = 0; t < 25000; t++) {
+            algorithm4(arrays[i], 0, 5 * (i + 1));
+        }
+        end = clock();
+        times[i][3] = (end, start) / (double) CLOCKS_PER_SEC;
+    }
+
+    ofstream outStream;
+    outStream.open("lucaswelton_phw_output.txt");
+    for (int i = 0; i < 19; i++) {
+        outStream << times[i][0] << "," << times[i][1] << "," << times[i][2] << "," << times[i][3] << endl;
+    }
+    outStream.close();
 }
